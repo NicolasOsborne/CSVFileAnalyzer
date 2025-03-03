cba23d7171ced79c8a1cd714549246cd452bc466
wbeyler <wilson.beyler@gmail.com>
2025-03-03 10:06:30 +0100

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import CSVUploader from './components/CSVUploader'
+import CSVConverter from "./components/CSVConverter.tsx";
 
 function App() {
   return (
-    <section className='home'>
-      <CSVUploader />
-    </section>
+      <Routes>
+          <Route path="/" element={<CSVUploader />} />
+          <Route path="/analyse" element={<CSVConverter />} />
+      </Routes>
   )
 }
 
