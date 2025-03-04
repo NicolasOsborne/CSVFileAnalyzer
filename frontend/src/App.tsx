78c29df082c817f416c896bcac52eddb52aae319
Nicolas Osborne <nho.osborne@gmail.com>
2025-03-04 14:10:51 +0100

    Connect frontend interface to backend api to send csv file to azure to analyze - Receive JSON analyze response - Save JSON to localStorage - Display results - Dynamic display of OK or ERROR depending on the files status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import CSVUploader from './components/CSVUploader'
-import CSVConverter from './components/CSVConverter.tsx'
 import NavigationButton from './components/NavigationButton.tsx'
+import CSVUploader from './components/CSVUploader'
+import CSVConverter, { CSVAnalysis } from './components/CSVConverter.tsx'
 
 function App() {
+  const [analysisResult, setAnalysisResult] = useState<CSVAnalysis | null>(null)
+  const [fileName, setFileName] = useState<string | null>(null)
   return (
     <>
       <NavigationButton />
       <Routes>
-        <Route path='/' element={<CSVUploader />} />
-        <Route path='/analyse' element={<CSVConverter />} />
+        <Route
+          path='/'
+          element={
+            <CSVUploader
+              setAnalysisResult={setAnalysisResult}
+              setFileName={setFileName}
+            />
+          }
+        />
+        <Route
+          path='/analyse'
+          element={
+            <CSVConverter analysisResult={analysisResult} fileName={fileName} />
+          }
+        />
       </Routes>
     </>
   )
