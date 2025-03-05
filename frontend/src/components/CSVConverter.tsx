aa9b95ab635b5f573514ff36ba62033968ce4d1f
Nicolas Osborne <nho.osborne@gmail.com>
2025-03-05 22:00:32 +0100

    Add send/receive analyze reacp by email

--- frontend/src/components/CSVConverter.tsx
+++ frontend/src/components/CSVConverter.tsx
@@ -4,6 +4,9 @@ import { IoIosArrowBack } from 'react-icons/io'
 import { toast } from 'react-toastify'
 import Spinner from './Spinner'
 
+import emailjs from '@emailjs/browser'
+import { IoMailUnreadOutline } from 'react-icons/io5'
+
 export type CSVAnalysis = {
   analysis: {
     price: StatValues
@@ -82,6 +85,118 @@ function CSVConverter({
     setDetailAnalyse(analyse)
   }
 
+  const sendByEmail = () => {
+    if (!detailAnalyse) return
+
+    const analysisBody = `
+      <h3 style="color: #aa65d3">Analyse des Données CSV: ${
+        detailAnalyse.name
+      }</h3>
+      <h4 style="color: #aa65d3">Analyse des Statistiques</h4>
+      <table style="width: 100%; border-collapse: collapse; border: 1px solid #696969; text-align: center;">
+        <thead>
+          <tr style="background-color: #e3c1f7">
+            <th style="padding: 0.5rem; border: 1px solid #696969;">Type</th>
+            <th style="padding: 0.5rem; border: 1px solid #696969;">Moyenne</th>
+            <th style="padding: 0.5rem; border: 1px solid #696969;">Médiane</th>
+            <th style="padding: 0.5rem; border: 1px solid #696969;">Écart-type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="padding: 0.5rem; border: 1px solid #696969; background-color: #e3c1f7">Prix</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.price.mean.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.price.median.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.price.standardDeviation.toFixed(
+              2
+            )}</td>
+          </tr>
+          <tr>
+            <td style="padding: 0.5rem; border: 1px solid #696969; background-color: #e3c1f7">Quantité</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.quantity.mean.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.quantity.median.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.quantity.standardDeviation.toFixed(
+              2
+            )}</td>
+          </tr>
+          <tr>
+            <td style="padding: 0.5rem; border: 1px solid #696969; background-color: #e3c1f7">Notes</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.score.mean.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.score.median.toFixed(
+              2
+            )}</td>
+            <td style="padding: 0.5rem; border: 1px solid #696969;">${detailAnalyse.content.analysis.score.standardDeviation.toFixed(
+              2
+            )}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h4 style="color: #aa65d3">Produits avec Erreurs</h4>
+      ${
+        Object.keys(detailAnalyse.content.errors).length > 0
+          ? `
+        <table style="width: 100%; border-collapse: collapse; border: 1px solid #696969; text-align: center;">
+          <thead>
+            <tr style="background-color: #e3c1f7">
+              <th style="padding: 0.5rem; border: 1px solid #696969;">ID</th>
+              <th style="padding: 0.5rem; border: 1px solid #696969;">Nom</th>
+              <th style="padding: 0.5rem; border: 1px solid #696969;">Prix</th>
+              <th style="padding: 0.5rem; border: 1px solid #696969;">Quantité</th>
+              <th style="padding: 0.5rem; border: 1px solid #696969;">Note Client</th>
+              <th style="padding: 0.5rem; border: 1px solid #696969;">Erreur</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${Object.values(detailAnalyse.content.errors)
+              .map(
+                (error: Product & { errorType?: string }) => `
+                <tr>
+                  <td style="padding: 0.5rem; border: 1px solid #696969;">${error.id}</td>
+                  <td style="padding: 0.5rem; border: 1px solid #696969;">${error.name}</td>
+                  <td style="padding: 0.5rem; border: 1px solid #696969;">${error.price}</td>
+                  <td style="padding: 0.5rem; border: 1px solid #696969;">${error.quantity}</td>
+                  <td style="padding: 0.5rem; border: 1px solid #696969;">${error.score}</td>
+                  <td style="padding: 0.5rem; border: 1px solid #696969; color: #ed4337; background-color: #fcd3d3">${error.errorType}</td>
+                </tr>`
+              )
+              .join('')}
+          </tbody>
+        </table>
+      `
+          : '<p style="color: #49ac49; font-weight: 600">✅ Aucun produit en erreur !</p>'
+      }
+    `
+
+    emailjs
+      .send(
+        import.meta.env.VITE_REACT_APP_EMAILJS_SERVICE_KEY,
+        import.meta.env.VITE_REACT_APP_EMAILJS_TEMPLATE_ID,
+        {
+          file_name: detailAnalyse.name,
+          message: analysisBody,
+        },
+        import.meta.env.VITE_REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        toast.success('Email récapitulatif envoyé avec succès !')
+      })
+      .catch(() => {
+        toast.error(
+          "Une erreur est survenue lors de l'envoi de l'email. Veuillez réessayer."
+        )
+      })
+  }
+
   return (
     <section className='csvConverter'>
       {loading && <Spinner />}
@@ -89,13 +204,22 @@ function CSVConverter({
         <>
           <h1 className='csvConverter_title'>Détails de l'analyse</h1>
           <div className='csvConverter_details'>
-            <button
-              className='csvConverter_details-backToHistory'
-              onClick={() => setShow(false)}
-            >
-              <IoIosArrowBack />
-              Retour à l'historique
-            </button>
+            <div className='csvConverter_details-buttons'>
+              <button
+                className='csvConverter_details-backToHistory'
+                onClick={() => setShow(false)}
+              >
+                <IoIosArrowBack />
+                Retour à l'historique
+              </button>
+              <button
+                className='csvConverter_details-sendByEmail'
+                onClick={sendByEmail}
+              >
+                <IoMailUnreadOutline />
+                Recevoir le récapitulatif par email
+              </button>
+            </div>
             {
               <div className='csvConverter_details-info'>
                 {detailAnalyse && (
